5f329ac8bd9e8f327c30406f70df2332cfb28950
jiang.chenguang <jiang.chenguang@outlook.com>
2024-07-21 14:42:56 +0800

    p35

--- spring-ioc-java-06/src/main/java/com/atguigu/config/JavaConfiguration.java
+++ spring-ioc-java-06/src/main/java/com/atguigu/config/JavaConfiguration.java
@@ -1,10 +1,36 @@
 package com.atguigu.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan("com.atguigu.ioc_01")
+@PropertySource("classpath:jdbc.properties")
 @Configuration
 public class JavaConfiguration {
 
+    @Value("${jdbc.url}")
+    public String url;
+
+    @Value("${jdbc.driverClassName}")
+    public String driverClassName;
+
+    @Value("${jdbc.username}")
+    public String username;
+
+    @Value("${jdbc.password}")
+    public String password;
+
+    @Bean
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(this.driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 }
