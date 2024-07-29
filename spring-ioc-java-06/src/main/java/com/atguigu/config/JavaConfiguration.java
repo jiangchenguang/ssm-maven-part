94af5d7ef856dad97b1ed4fadbf906fd4065613d
jiang.chenguang <jiang.chenguang@outlook.com>
2024-07-29 22:25:44 +0800

    p36

--- spring-ioc-java-06/src/main/java/com/atguigu/config/JavaConfiguration.java
+++ spring-ioc-java-06/src/main/java/com/atguigu/config/JavaConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @ComponentScan("com.atguigu.ioc_01")
 @PropertySource("classpath:jdbc.properties")
@@ -24,7 +25,7 @@ public class JavaConfiguration {
     @Value("${jdbc.password}")
     public String password;
 
-    @Bean
+    @Bean(name = "ergouzi")
     public DruidDataSource dataSource() {
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(url);
@@ -32,5 +33,24 @@ public class JavaConfiguration {
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         return dataSource;
+    }
+
+    @Bean(name = "lvdandan")
+    public DruidDataSource dataSource1() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(this.driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DruidDataSource ergouzi) {
+        JdbcTemplate template = new JdbcTemplate();
+        // 如果没有对应类型的ioc组件，则报错
+        // 如果有多个对应类型的ioc组件，必须通过形参名指定对应的beanid
+        template.setDataSource(ergouzi);
+        return template;
     }
 }
