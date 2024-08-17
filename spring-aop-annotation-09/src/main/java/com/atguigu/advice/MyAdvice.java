4310179fef94b16a31371e9365a1d3c3bd4ca7c6
jiang.chenguang <jiang.chenguang@outlook.com>
2024-08-17 09:57:32 +0800

    p48

---
+++ spring-aop-annotation-09/src/main/java/com/atguigu/advice/MyAdvice.java
@@ -0,0 +1,30 @@
+package com.atguigu.advice;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAdvice {
+
+    @Before("execution(* com..impl.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        String simpleName = joinPoint.getTarget().getClass().getSimpleName();
+        String name = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+    }
+
+    @AfterReturning(value = "execution(* com..impl.*.*(..))", returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+
+    }
+
+    @After("execution(* com..impl.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+    }
+
+    @AfterThrowing(value = "execution(* com..impl.*.*(..))", throwing = "throwable")
+    public void afterThrowing(JoinPoint joinPoint, Throwable throwable) {}
+}
